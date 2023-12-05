20d83d7997ecd4ed42f9509632c0b0d10ef715d5
wojtekpirog <114958949+wojtekpirog@users.noreply.github.com>
2023-12-06 00:34:06 +0100

    Add responsive navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,34 @@
+import { useState } from 'react';
 import header from './header.module.css';
+import coinImage from '../../assets/images/coin.svg';
 
 function Header() {
+  const [collapse, setCollapse] = useState(false);
+
+  const toggleNavbar = () => {
+    setCollapse(!collapse);
+  }
+
   return (
     <header>
-      <h1 className={header.pageHeading}>Siemaneczko 😊</h1>
+      <nav className={`${header.pageHeader} ${header.flex}`}>
+        <div className={`${header.navbar} ${header.flex}`}>
+          <div className={`${header.pageLogo} ${header.flex}`}>
+            <img src={coinImage} alt="page logo icon" />
+            <span>exchange.me</span>
+          </div>
+          <button type="button" className={header.navbarToggler} onClick={toggleNavbar}>
+            <div></div>
+            <div></div>
+            <div></div>
+          </button>
+        </div>
+        <ul className={header.navbarLinks}>
+          <li><a href="#" target="_blank" draggable="true">About Me</a></li>
+          <li><a href="#" target="_blank" draggable="true">Features</a></li>
+          <li><a href="#" target="_blank" draggable="true">Contact</a></li>
+        </ul>
+      </nav>
     </header>
   );
 }
